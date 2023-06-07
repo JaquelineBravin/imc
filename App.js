437d595783905478a40959b526692f2dbb0158ca
JaquelineBravin <111389555+JaquelineBravin@users.noreply.github.com>
2023-06-06 20:35:52 -0400

    arrumando a lógica

--- App.js
+++ App.js
@@ -1,63 +1,56 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   const [peso, setPeso] = useState("");
   const [altura, setAltura] = useState("");
-  const [resultado, setResultado] = useState("");
+  const [resultado, setResultado] = useState(0);
+  const [botaoClicado, setBotaoClicado] = useState(false);
 
   const calcular = () => {
-    const altura = parseFloat(altura / 100, 2);
-    const peso = parseFloat(peso);
+    const alt = parseFloat(altura / 100, 2);
+    const pes = parseFloat(peso);
 
-    const resultado = peso / (altura * altura)
-    setResultado(`Seu IMC é ${resultado.toFixed(2)}`)
-
-    return (
-      <View className="flex-1 items-center bg-red-500 justify-center">
-        <Text className="text-lg font-bold">Wellcome to my App!</Text>
-        <Text className="text-lg font-bold">Calcule seu IMC</Text>
-        <Text>- By: Jaque</Text>
-        <StatusBar style="auto" />
-        <Text>Altura:</Text>
-        <TextInput
-          className="bg-blue-500 w-3/6 m-5 p-5"
-          keyboardType="number-pad"
-          value={altura}
-          onChangeText={(tex) => setAltura(tex)}
-          placeholder="altura (cm)"
-        />
-        <Text>Peso:</Text>
-        <TextInput
-          className="bg-blue-500 w-3/6 m-5 p-5"
-          keyboardType="number-pad"
-          value={peso}
-          onChangeText={(tex) => setPeso(tex)}
-          placeholder="peso"
-        />
-        +
-        <TouchableOpacity
-          className="bg-blue-500 p-10 border-fuchsia-50 text-zinc-950"
-          onPress={calcular}
-        >
-          <Text>Calcular</Text>
-        </TouchableOpacity>
-      </View>
-    );
+    const res = pes / (alt * alt);
+    console.log("peso", pes);
+    return setResultado(res.toFixed(2));
   };
+
+  const handleClick = () => {
+    setBotaoClicado(true);
+    calcular();
+  };
+
+  return (
+    <View className="flex-1 items-center bg-whithe justify-center">
+      <View>{!!setBotaoClicado && <Text>Seu IMC é {resultado}</Text>}</View>
+      <Text className="text-lg font-bold">Wellcome to my App!</Text>
+      <Text className="text-lg font-bold">Calcule seu IMC</Text>
+      <Text>- By: Jaque</Text>
+      <StatusBar style="auto" />
+      <Text>Altura:</Text>
+      <TextInput
+        className="bg-blue-200 w-3/6 m-5 p-5 rounded"
+        keyboardType="number-pad"
+        value={altura}
+        onChangeText={(tex) => setAltura(tex)}
+        placeholder="altura (cm)"
+      />
+      <Text>Peso:</Text>
+      <TextInput
+        className="bg-blue-200 w-3/6 m-5 p-5 rounded"
+        keyboardType="number-pad"
+        value={peso}
+        onChangeText={(tex) => setPeso(tex)}
+        placeholder="peso"
+      />
+      <TouchableOpacity
+        className="bg-blue-400 p-10 border-fuchsia-50 text-zinc-950 rounded"
+        onPress={handleClick}
+      >
+        <Text>Calcular</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
-
-/*
-const IMC = () => {
-    const calculoIMC = () => {
-      if (peso && altura) {
-        const IMC = peso / Math.pow(altura / 100, 2);
-        setResultado(`Seu IMC é ${bmi.toFixed(2)}`);
-      } else {
-        setResultado("Preencha os campos corretamente!");
-      }
-    };
-  };
-
-  */
